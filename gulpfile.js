94d7ca7a13a8d6c55f04ec415e97dadc2ef06134
ChrisIngoe <chris.ingoe@gmail.com>
2019-11-18 22:44:44 +0000

    make layout responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
+// Static server
 gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  });
-  gulp.watch(['./*.html', './css/*.css']).on('change', browserSync.reload);
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch(["./*.html", "./css/*.css"]).on('change', browserSync.reload);
 });
